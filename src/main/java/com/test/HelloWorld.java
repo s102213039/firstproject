6fc727c3b07dc7f8a1262a1a4656cb7ee2bb914d
Y.Y. Chiu <s102213039@ncnu.edu.tw>
2019-09-09 14:17:57 +0800

    Java and Kotlin variable,data type

--- src/main/java/com/test/HelloWorld.java
+++ src/main/java/com/test/HelloWorld.java
@@ -3,9 +3,16 @@ package com.test;
 public class HelloWorld {
     public static void main(String[] args) {
 //        System.out.println("Hello World");
-    Person p = new Person();
-    p.hello();
+        Person p = new Person();
+        p.hello();
     }
 
+    int age = 19;
+    char c = 'A';
+    byte b = 120;
+    float weight = 66.5f;
+    double height = 1.7;
+    boolean adult = true;
+    String name = "Tom";
 
 }
--- src/main/kotlin/com/test/Hello.kt
+++ src/main/kotlin/com/test/Hello.kt
@@ -4,10 +4,16 @@ fun main() {
 //    println("Hello World")
     var h = Human()
     h.hello()
+
+    var age = 19
+    age = 20
+    var weight = 66.5
+    val name: String
+    name = "Tom"
 }
 
-class Human{
-    fun hello(){
+class Human {
+    fun hello() {
         println("Hello Kotlin")
     }
 }
